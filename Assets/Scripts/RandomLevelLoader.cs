4fdd50ae871db62fd90e0acad5de5b1ba9142866
Georgie Rene <georgiana.wright99@gmail.com>
2025-07-16 02:18:19 -0500

    added randomize maze script, main menu for testing, and added scenes in build info

---
+++ Assets/Scripts/RandomLevelLoader.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class RandomLevelLoader : MonoBehaviour
+{
+    public static RandomLevelLoader Instance;
+
+    public List<string> levelSceneNames = new List<string> { "Maze1", "Maze2", "Maze3" };
+    private List<string> unplayedLevels = new List<string>();
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            ResetUnplayedLevels();
+        }
+        else
+        {
+            Destroy(gameObject); // Prevent duplicates
+        }
+    }
+
+    public void LoadRandomLevel()
+    {
+        if (unplayedLevels.Count == 0)
+        {
+            Debug.Log("All mazes played. Resetting...");
+            ResetUnplayedLevels();
+        }
+
+        int index = Random.Range(0, unplayedLevels.Count);
+        string chosenLevel = unplayedLevels[index];
+        unplayedLevels.RemoveAt(index);
+
+        Debug.Log("Loading: " + chosenLevel);
+        SceneManager.LoadScene(chosenLevel);
+    }
+
+    private void ResetUnplayedLevels()
+    {
+        unplayedLevels = new List<string>(levelSceneNames);
+    }
+}
