f411a6eba4dd3c311e7351ad344c2a76e723364d
JulianCT05 <juliantolbertll@gmail.com>
2025-07-17 11:35:17 -0400

    Finishing up touches
    Maze 2 has little sprites

--- Assets/Scripts/ColllectablesScripts.cs
+++ Assets/Scripts/ColllectablesScripts.cs
@@ -4,12 +4,23 @@ public class CollectableItem : MonoBehaviour
 {
     public string itemName;
     public Sprite itemSprite;
+    public AudioClip collectSound; 
+    public float soundVolume = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+          
             CollectableManager.Instance.CollectItem(itemName, itemSprite);
+
+            
+            if (collectSound != null)
+            {
+                AudioSource.PlayClipAtPoint(collectSound, transform.position, soundVolume);
+            }
+
+            
             Destroy(gameObject);
         }
     }
